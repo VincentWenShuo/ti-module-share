e91576a728c5778fa7dfe68792fdc54734c95b54
VincentWenShuo <vincent.wenshuo@Vincent-WenShuos-MacBook-Pro.local>
2014-07-21 10:12:15 +0100

    ti-module-share
    Titanium IOS module for native share

---
+++ build/ti-module-share.build/Release-iphonesimulator/ti-module-share.build/Objects-normal/i386/TiModuleShareModuleAssets.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/vincent.wenshuo/Documents/Titanium_Studio_Workspace/ti-module-share/Classes/TiModuleShareModuleAssets.m \
+  /Users/vincent.wenshuo/Documents/Titanium_Studio_Workspace/ti-module-share/Classes/TiModuleShareModuleAssets.h
